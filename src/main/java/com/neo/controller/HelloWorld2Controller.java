85c77cfcea8ac47a9a8ddf3347e19e5b9b835573
bethebluewonder <1074540448@qq.com>
2020-06-10 14:29:21 +0800

    修改HelloWorld2Controller

--- src/main/java/com/neo/controller/HelloWorld2Controller.java
+++ src/main/java/com/neo/controller/HelloWorld2Controller.java
@@ -1,13 +1,13 @@
 package com.neo.controller;
 
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld2Controller {
 
     @RequestMapping("/hello")
     public String index() {
-        return "Hello World22222222ooooo";
+        return "Hello World!!!!";
     }
 }
